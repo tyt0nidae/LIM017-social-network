c98f910b1c164dda0fb7e2bc56566080d9c85814
tyt0nidae <mcnavarro957@gmail.com>
2022-03-28 19:37:09 -0500

    Templates de Register, Login y Home

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
-// Este es el punto de entrada de tu aplicacion
+import { Home } from './lib/Home.js';
+import { Register } from './lib/Register.js';
+import { Login } from './lib/Login.js';
 
-import { myFunction } from './lib/index.js';
+const rootDiv = document.getElementById('root');
 
-myFunction();
+const routes = {
+  '/': Home,
+  '/register': Register,
+  '/login': Login,
+};
+
+const onNavigate = (pathname) => {
+window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname
+)
+rootDiv.innerHTML= routes[pathname]
+};
+
+window.onpopstate = () => {
+    rootDiv.innerHTML = routes[window.location.pathname]
+  }
+
+const component = routes[window.location.pathname];
+rootDiv.innerHTML = component
